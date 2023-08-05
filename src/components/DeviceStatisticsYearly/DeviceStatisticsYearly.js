159f5a6042f6644756665782bc6ef9c151be64f4
Nikol Sapozhnikov <87708403+NikolSap@users.noreply.github.com>
2023-08-05 21:12:11 +0300

    with style

--- src/components/DeviceStatisticsYearly/DeviceStatisticsYearly.js
+++ src/components/DeviceStatisticsYearly/DeviceStatisticsYearly.js
@@ -48,62 +48,49 @@ const DeviceStatisticYearly = ({ route, navigation }) => {
     <ScrollView>
       <Text style={styles.item_title}>Device Name:</Text>
       <Text style={styles.item_name}>{name}</Text>
-      <BarChart
-        data={{
-          labels: [
-            "J",
-            "F",
-            "M",
-            "A",
-            "M",
-            "Jun",
-            "Jul",
-            "A",
-            "S",
-            "O",
-            "N",
-            "D",
-          ],
-          datasets: [
-            {
-              data: data,
+      <View style={styles.chartContainer}>
+        <BarChart
+          data={{
+            labels: ["J", "F", "M", "A", "M", "Jun", "Jul", "A", "S", "O", "N", "D"],
+            datasets: [
+              {
+                data: data,
+              },
+            ],
+          }}
+          fromZero={true}
+          segments={4}
+          width={Dimensions.get("window").width - 40} // Adjusted width to give some margin on both sides
+          height={400} // You can increase the height if needed
+          verticalLabelRotation={40}
+          chartConfig={{
+            backgroundColor: "#FF3399",
+            backgroundGradientFrom: "#99FFFF",
+            backgroundGradientTo: "#FF3399",
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+              flex: 1,
+              justifyContent: "center",
+              strokeWidth: 2,
+              barPercentage: 0.5,
             },
-          ],
-        }}
-        fromZero={true}
-        segments={4}
-        width={Dimensions.get("window").width}
-        height={400}
-        verticalLabelRotation={40}
-        chartConfig={{
-          backgroundColor: "#FF3399",
-          backgroundGradientFrom: "#99FFFF",
-          backgroundGradientTo: "#FF3399",
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-          style: {
-            borderRadius: 16,
-            flex: 1,
-            justifyContent: "center",
-            strokeWidth: 2, // optional, default 3
-            barPercentage: 0.5,
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#ffa726",
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 2,
-          borderRadius: 2,
-          paddingTop: 40,
-          height: 400,
-          paddingBottom: 40,
-        }}
-      />
-
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 2,
+            borderRadius: 2,
+            paddingTop: 40,
+            paddingBottom: 40,
+          }}
+        />
+      </View>
       {/* <Text style={styles.item_usage}>device usage:</Text>
       <Text style={styles.item_name}>{usage}</Text> */}
     </ScrollView>
